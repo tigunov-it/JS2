20cb80bf1b3c4c4e98cdfdd9844d5f5c2ecc59aa
Yuriy Tigunov <192215@gmail.com>
2021-04-06 15:10:20 +0800

    lesson3 v0.1b

--- scripts/script.js
+++ scripts/script.js
@@ -1,49 +1,27 @@
-//Прежде всего у нас есть товар, создадим для него класс GoodsItem.
-// На текущем этапе у него будет два свойства – title и price – и один метод render, который будет возвращать html-разметку.
-
-class GoodsItem {
-    constructor(title, price) {
-        this.title = title;
-        this.price = price;
-    }
-
-    render() {
-        // return `<div class="goods-item"><h3>${this.title}</h3><p>${this.price}</p></div>`;
-        return `<div class="col">
-        <div class="card">
-            <img src="https://picsum.photos/200" class="card-img-top" alt="...">
-            <a href="#"> <div class="card-body">
-                <h5 class="card-title">${this.title}</h5>
-                <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                <p class="card-text"><small class="text-muted">Цена: ${this.price}</small></p>
-            </div></a>
-        </div>
-    </div>`
-    }
-}
-
-//список товаров
-class GoodsList {
-    constructor() {
+class ProductsList {
+    constructor(container = '.products') {
+        this.container = container;
         this.goods = [];
+        this.allProducts = [];
+        this._fetchProducts();
     }
 
-    fetchGoods() {
+    _fetchProducts() {
         this.goods = [
-            {title: 'Shirt', price: 150},
-            {title: 'Socks', price: 50},
-            {title: 'Jacket', price: 350},
-            {title: 'Shoes', price: 250},
+            {id: 1, title: 'Notebook', price: 2000},
+            {id: 2, title: 'Mouse', price: 20},
+            {id: 3, title: 'Keyboard', price: 200},
+            {id: 4, title: 'Gamepad', price: 50},
         ];
     }
 
     render() {
-        let listHtml = '';
-        this.goods.forEach(good => {
-            const goodItem = new GoodsItem(good.title, good.price);
-            listHtml += goodItem.render();
-        });
-        document.querySelector('.goods-list').innerHTML = listHtml;
+        const block = document.querySelector(this.container);
+        for (let product of this.goods) {
+            const productObj = new ProductItem(product);
+            this.allProducts.push(productObj);
+            block.insertAdjacentHTML('beforeend', productObj.render())
+        }
     }
 
     sum() { // метод для вычисления суммы всех товаров
@@ -53,53 +31,70 @@ class GoodsList {
         }
         console.log(sum);
     }
+
 }
 
-class Cart {
-    addToCart() {   //Добавить в корзину
+class ProductItem {
+    constructor(product, img = 'https://picsum.photos/200') {
+        this.title = product.title;
+        this.price = product.price;
+        this.id = product.id;
+        this.img = img;
 
     }
-    deleteFromCart() { //Удалить из корзины
 
+    render() {
+        // return `<div class="goods-item"><h3>${this.title}</h3><p>${this.price}</p></div>`;
+        return `<div class="products product-item" id="${this.id}">
+        <div class="card">
+            <img src="https://picsum.photos/200" class="card-img-top" alt="...">
+            <a href="#"> <div class="card-body">
+                <h5 class="card-title">${this.title}</h5>
+                <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                <p class="card-text"><small class="text-muted">Цена: ${this.price}</small></p>
+                <button class="buy-btn button">Купить</button>
+            </div></a>
+        </div>
+    </div>`
     }
-    sum() { //сумма товаров в корзине
 
-    }
 }
 
-
-const list = new GoodsList();
-list.fetchGoods();
+let list = new ProductsList();
 list.render();
 list.sum();
 
+class Cart {
+    constructor(container = ".cart-menu") {
+       this.container = container;
+    }
 
-// const goods = [
-//     {title: 'Shirt', price: 150},
-//     {title: 'Socks', price: 50},
-//     {title: 'Jacket', price: 350},
-//     {title: 'Shoes', price: 250},
-//     {price: 250},
-//     {title: 'Shoes'},
-// ];
-//
-// const renderGoodsItem = (title = 'Товар', price = "Уточняйте у менеджера") => {
-//     // return `<div class="goods-item"><h3>${title}</h3><p>${price}</p></div>`;
-//         return `<div class="col">
-//         <div class="card">
-//             <img src="https://picsum.photos/200" class="card-img-top" alt="...">
-//             <a href="#"> <div class="card-body">
-//                 <h5 class="card-title">${title}</h5>
-//                 <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-//                 <p class="card-text"><small class="text-muted">Цена: ${price}</small></p>
-//             </div></a>
-//         </div>
-//     </div>`
-// };
-//
-// const renderGoodsList = (list) => {
-//     let goodsList = list.map(item => renderGoodsItem(item.title, item.price));
-//     document.querySelector('.goods-list').innerHTML = goodsList.join('');
-// }
-//
-// renderGoodsList(goods);
+    addGoods(item) {
+        const block = document.querySelector(this.container);
+        block.insertAdjacentHTML('beforeend', `<p> ${item} </p>` );
+        }
+
+    removeGoods() {
+
+    }
+
+    changeGoods() {
+
+    }
+}
+
+let cart = new Cart();
+listener();
+
+function listener () {
+    const buttonItems = document.querySelectorAll('.buy-btn');
+    for (let i = 0; i < buttonItems.length; i++) {
+        let button = buttonItems[i];
+        button.addEventListener('click', function (event) {
+
+            cart.addGoods(event.target.parentElement.innerText);
+            console.log(event.target);
+            console.log(event.target.parentNode);
+        })
+    }
+}
